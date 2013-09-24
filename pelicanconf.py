c61075feffbf298c3f6788494ada1a656ab56267
Blixa Morgan <blixa@projectmakeit.com>
2013-09-24 14:27:41 -0500

    jenkins push

---
+++ pelicanconf.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Blixa Morgan'
+SITENAME = u'Project Make It'
+SITEURL = ''
+TWITTER_USERNAME = 'blixam'
+
+TIMEZONE = 'Europe/Paris'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+#FEED_ALL_ATOM = None
+#CATEGORY_FEED_ATOM = None
+#TRANSLATION_FEED_ATOM = None
+
+# Blogroll
+LINKS =  (('Tulsa Open Souce Hardware', 'http://tulsaopensource.com/'),
+          ('Tulsa Web Devs', 'http://tulsawebdevs.org'))
+
+# Social widget
+SOCIAL = (('Google+', 'https://plus.google.com/103019962147079506840'),
+          ('Youtube', 'https://www.youtube.com/user/ProjectMakeIt'),
+          ('Github', 'http://github.com/blister75/'),
+          ('Gittip', 'http://gittip.com/blister75/'),
+          ('Twitter', 'http://twitter.com/blixam'))
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
+DEFAULT_DATE = 'fs'
+
+DISPLAY_CATEGORIES_ON_MENU = False
+
+#PDF_GENERATOR = True
